bd310f856309e2f186c03f443c103a9d12c58430
Vaishali Raut <142431370+v-aishuu2510@users.noreply.github.com>
2024-03-06 02:45:16 +0530

    task1 from Dev1

--- GITProj1/src/main/java/com/nt/main/AppTest.java
+++ GITProj1/src/main/java/com/nt/main/AppTest.java
@@ -1,11 +1,14 @@
 package com.nt.main;
 
+import com.nt.service.ArithmeticOperations;
+
 public class AppTest {
 
 	public static void main(String[] args) 
 	{
-		
-
+		ArithmeticOperations ar=new ArithmeticOperations();
+		System.out.println("Sum: "+ar.sum(100, 200));
+		System.out.println("Sub: "+ar.sub(100, 200));
 	}
 
 }
